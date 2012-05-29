3ecb99f8b5f8ce2c457bd98c5ddd2e5af8b92ec9
Toshiyuki Hirooka <toshi.hirooka@gmail.com>
2012-05-29 15:46:00 +0900

    removes not required file because replaced to vundle

--- get_files.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-mkdir ~/.vim
-mkdir ~/.vim/colors/
-#wget -O ~/.vim/colors/desert256.vim http://www.vim.org/scripts/download_script.php?src_id=4055
-wget -O ~/.vim/colors/desert256.vim https://github.com/vim-scripts/desert256.vim/raw/28218ba05f77003e720ede1e74efaa17cc3e9051/colors/desert256.vim --no-check-certificate
