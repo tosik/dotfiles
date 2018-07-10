8a28f17b9eb9bbdd07208d5c2c3647f4d745125b
Toshiyuki Hirooka <toshi.hirooka@gmail.com>
2018-07-11 08:52:41 +0900

    add .tmux.conf

--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.ctags ~/.ctags
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
