c909d7b99743e81d442649b54198baa19127c85e
Toshiyuki Hirooka <toshi.hirooka@gmail.com>
2018-12-08 23:28:22 +0900

    removes tmux and screen config

--- setup.sh
+++ setup.sh
@@ -7,6 +7,3 @@ ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 mkdir -p ~/.config/nvim
 ln -sf ~/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
 
-mkdir -p ~/bin
-ln -sf ~/dotfiles/tmux-pane-border ~/bin/tmux-pane-border
-chmod a+x ~/bin/tmux-pane-border
